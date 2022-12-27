ee90f5b1b6d2752ab0f8fdfb00f4cca0fac0b409
TJC <banzzak00@gmail.com>
2022-12-27 09:45:51 +0900

    No Paths return for pre-rendering

--- pages/[keyword].tsx
+++ pages/[keyword].tsx
@@ -145,6 +145,10 @@ interface Params extends ParsedUrlQuery {
 export const getStaticPaths:GetStaticPaths<Params> = async () => { 
   const paths = await getPrerenderingKeywords()
   return {
+    paths: [],
+    fallback: 'blocking'
+  }
+  return {
     paths,
     fallback: 'blocking'
   }
