809c2ec778dab187cc48072c4e45feaa9aae20f8
TJC <banzzak00@gmail.com>
2022-12-29 13:09:18 +0900

    Enclose title text in span to improve hydration

--- pages/[keyword].tsx
+++ pages/[keyword].tsx
@@ -29,7 +29,7 @@ export default function DetailPage({hasSearchResult, keyword, recommendedKeyword
   return (
     <>
       <Head>
-        <title> {keyword} 추천 </title>
+        <title><span> {keyword} 추천 </span></title>
         <meta name="description" content={metaDescription}/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
